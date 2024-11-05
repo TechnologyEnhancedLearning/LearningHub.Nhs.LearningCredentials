b5f0c16f1b3ceb3edd158bbb68ea897efa70b46d
Colin Beeby <colin.beeby@hee.nhs.uk>
2024-11-05 10:23:26 +0000

    Added LearningCrednetialsSubnetId to Prod actions yml file

--- LearningHub.Nhs.LearningCredentials.Database/Scripts/ReleaseScripts/Populate_ClientSystems.sql
+++ LearningHub.Nhs.LearningCredentials.Database/Scripts/ReleaseScripts/Populate_ClientSystems.sql
@@ -2,18 +2,18 @@
 
 IF NOT EXISTS (SELECT 1 FROM [system].[ClientSystem] WHERE Id = 1)
 BEGIN
-	INSERT INTO [system].[ClientSystem] ([Id],[Name],[Deleted],[CreateUserId],[CreateDate],[AmendUserId],[AmendDate])
-	VALUES (1, 'elearning for healthcare', 0, 4, SYSDATETIMEOFFSET(), 4, SYSDATETIMEOFFSET())
+	INSERT INTO [system].[ClientSystem] ([Id],[Name],[Origin],[Provider],[Deleted],[CreateUserId],[CreateDate],[AmendUserId],[AmendDate])
+	VALUES (1, 'elearning for healthcare', 'elfh Hub', 'NHS England TEL', 0, 4, SYSDATETIMEOFFSET(), 4, SYSDATETIMEOFFSET())
 END
 IF NOT EXISTS (SELECT 1 FROM [system].[ClientSystem] WHERE Id = 2)
 BEGIN
-	INSERT INTO [system].[ClientSystem] ([Id],[Name],[Deleted],[CreateUserId],[CreateDate],[AmendUserId],[AmendDate])
-	VALUES (2, 'Learning Hub', 0, 4, SYSDATETIMEOFFSET(), 4, SYSDATETIMEOFFSET())
+	INSERT INTO [system].[ClientSystem] ([Id],[Name],[Origin],[Provider],[Deleted],[CreateUserId],[CreateDate],[AmendUserId],[AmendDate])
+	VALUES (2, 'Learning Hub', 'Learning Hub', 'NHS England TEL', 0, 4, SYSDATETIMEOFFSET(), 4, SYSDATETIMEOFFSET())
 END
 GO
 IF NOT EXISTS (SELECT 1 FROM [system].[ClientSystem] WHERE Id = 3)
 BEGIN
-	INSERT INTO [system].[ClientSystem] ([Id],[Name],[Deleted],[CreateUserId],[CreateDate],[AmendUserId],[AmendDate])
-	VALUES (3, 'Digital Learning Solutions', 0, 4, SYSDATETIMEOFFSET(), 4, SYSDATETIMEOFFSET())
+	INSERT INTO [system].[ClientSystem] ([Id],[Name],[Origin],[Provider],[Deleted],[CreateUserId],[CreateDate],[AmendUserId],[AmendDate])
+	VALUES (3, 'Digital Learning Solutions', 'Digital Learning Solutions', 'NHS England TEL', 0, 4, SYSDATETIMEOFFSET(), 4, SYSDATETIMEOFFSET())
 END
 GO
